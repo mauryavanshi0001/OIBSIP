b885c2d46c2626791409da72597fa225cb558ecd
Ashutosh Maurya <94228402+mauryavanshi0001@users.noreply.github.com>
2023-08-15 13:46:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-let displayValue = '';
-
-function appendToDisplay(value) {
-  displayValue += value;
-  document.getElementById('display').value = displayValue;
-}
-
-function calculate() {
-  try {
-    displayValue = eval(displayValue);
-    document.getElementById('display').value = displayValue;
-  } catch (error) {
-    document.getElementById('display').value = 'Error';
-    setTimeout(clearDisplay, 1000);
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+    
+    if (taskInput.value !== '') {
+      const taskText = taskInput.value;
+      const taskItem = document.createElement('li');
+      taskItem.innerHTML = `
+        <span>${taskText}</span>
+        <button class="delete-button" onclick="removeTask(this)">Delete</button>
+      `;
+      taskList.appendChild(taskItem);
+      
+      taskInput.value = '';
+    }
   }
-}
-
-function clearDisplay() {
-  displayValue = '';
-  document.getElementById('display').value = '';
-}
+  
+  function removeTask(button) {
+    const taskList = document.getElementById('taskList');
+    const taskItem = button.parentNode;
+    taskList.removeChild(taskItem);
+  }
+  
